d17ab96bef7794db7d0ec36aef6465c6ac4fa1e7
Sushant-Coder-01 <sushantmangore45@gmail.com>
2025-07-19 21:04:12 +0530

    feat: update index.html and package files for 3D portfolio setup; add logo and initial styles

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,14 @@
+import { Canvas } from "@react-three/fiber";
+import React from "react";
+
 function App() {
-  return <>Hii</>;
+  return (
+    <React.Fragment>
+      <Canvas
+        style={{ width: "full", height: "100dvh", backgroundColor: "black" }}
+      ></Canvas>
+    </React.Fragment>
+  );
 }
 
 export default App;
