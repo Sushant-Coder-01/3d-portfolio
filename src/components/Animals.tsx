965a316089255cab33ac242c722a461d2df7ada5
Sushant-Coder-01 <sushantmangore45@gmail.com>
2025-08-24 13:59:19 +0530

    remove unused var

--- src/components/Animals.tsx
+++ src/components/Animals.tsx
@@ -1,4 +1,4 @@
-import React, { useRef, useEffect, useMemo } from "react";
+import { useRef, useEffect, useMemo } from "react";
 import { useGLTF, useAnimations } from "@react-three/drei";
 import { Group } from "three";
 
